79c0c941dc71b80ecb29ee54e37e207cd7f37484
Stefano <stefanop21@outlook.es>
2024-02-08 18:46:26 -0500

    install(repo): Material UI

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,5 +1,10 @@
 import { AppRouter } from './router/AppRouter';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 export const JournalApp = () => {
   return <AppRouter />;
 };
