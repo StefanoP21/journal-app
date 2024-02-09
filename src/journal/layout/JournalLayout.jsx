dfb410ff7d6e28d25edd217d2d01c5a240bf7a0c
Stefano <stefanop21@outlook.es>
2024-02-09 18:44:25 -0500

    create(src/journal/layout/): Componente JournalLayout.jsx

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,19 @@
+import { Box } from '@mui/material';
+
+const drawerWidth = 240;
+
+export const JournalLayout = ({ children }) => {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      {/* <NavBar /> */}
+
+      {/* <SideBar /> */}
+
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        {/* <Toolbar /> */}
+
+        {children}
+      </Box>
+    </Box>
+  );
+};
