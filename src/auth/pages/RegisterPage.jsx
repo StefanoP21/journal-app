315389a98d73caf91dfc0ef19211533b3aafeb7e
Stefano <stefanop21@outlook.es>
2024-02-09 18:37:01 -0500

    update(src/auth/pages): Diseño de RegisterPage.jsx

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,3 +1,58 @@
+import { Link as RouterLink } from 'react-router-dom';
+import { Google } from '@mui/icons-material';
+import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { AuthLayout } from '../layout/AuthLayout';
+
 export const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <AuthLayout title="Crear cuenta">
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Nombre"
+              type="text"
+              placeholder="Stefano Palomino"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Correo electrónico"
+              type="email"
+              placeholder="stefanop21@outlook.es"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              placeholder="********"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mt: 2, mb: 1 }}>
+            <Grid item xs={12}>
+              <Button variant="contained" type="submit" fullWidth>
+                <Typography variant="button">Registrarse</Typography>
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" justifyContent="end">
+            <Typography sx={{ mr: 1 }} color="text.secondary">
+              ¿Ya tienes una cuenta?
+            </Typography>
+            <Link component={RouterLink} color="inherit" to="/auth/login">
+              Ingresar
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
 };
