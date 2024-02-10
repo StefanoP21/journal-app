25bea0802c8d19e17bb70f322b4f1c6d98dd2b29
Stefano <stefanop21@outlook.es>
2024-02-09 20:52:45 -0500

    create(src/journal/components): Componente SideBar.jsx

---
+++ src/journal/components/SideBar.jsx
@@ -0,0 +1,56 @@
+import { TurnedInNot } from '@mui/icons-material';
+import {
+  Box,
+  Divider,
+  Drawer,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+
+export const SideBar = ({ drawerWidth }) => {
+  return (
+    <Box
+      component="nav"
+      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+    >
+      <Drawer
+        varient="permanent" // temporary
+        open
+        sx={{
+          display: { xs: 'block' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Stefano Palomino
+          </Typography>
+        </Toolbar>
+        <Divider />
+
+        <List>
+          {['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'].map((text) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <TurnedInNot />
+                </ListItemIcon>
+
+                <Grid container>
+                  <ListItemText primary={text} />
+                  <ListItemText secondary={text} />
+                </Grid>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  );
+};
