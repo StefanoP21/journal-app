1586fec75e9030fe62bb5951847d69584af051b7
Stefano <stefanop21@outlook.es>
2024-02-09 19:17:28 -0500

    update(src/journal/layou): Componente JournalLayout.jsx

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,11 +1,12 @@
 import { Box } from '@mui/material';
+import { NavBar } from '../components';
 
 const drawerWidth = 240;
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex' }}>
-      {/* <NavBar /> */}
+      <NavBar drawerWidth={drawerWidth} />
 
       {/* <SideBar /> */}
 
