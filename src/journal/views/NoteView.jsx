ac6c8db1c07921beddcb69ed38df9b5ec1e1ca5d
Stefano <stefanop21@outlook.es>
2024-02-09 21:36:10 -0500

    create(src/journal/views): Vista NoteView.jsx

---
+++ src/journal/views/NoteView.jsx
@@ -0,0 +1,51 @@
+import { SaveOutlined } from '@mui/icons-material';
+import { Button, Grid, TextField, Typography } from '@mui/material';
+import { ImageGallery } from '../components';
+
+export const NoteView = () => {
+  return (
+    <Grid
+      container
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      sx={{ mb: 1 }}
+    >
+      <Grid item>
+        <Typography fontSize={39} fontWeight="light">
+          Note
+        </Typography>
+      </Grid>
+
+      <Grid item>
+        <Button color="primary" sx={{ padding: 2 }}>
+          <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
+          Guardar
+        </Button>
+      </Grid>
+
+      <Grid container>
+        <TextField
+          type="text"
+          variant="filled"
+          placeholder="Ingrese un título"
+          label="Título"
+          sx={{ border: 'none', mb: 1 }}
+          fullWidth
+        />
+
+        <TextField
+          type="text"
+          variant="filled"
+          placeholder="¿Qué pasó hoy?"
+          label="Descripción"
+          fullWidth
+          multiline
+          minRows={5}
+        />
+      </Grid>
+
+      <ImageGallery />
+    </Grid>
+  );
+};
