31403f8da481fa432a01c6e753eb4df6789c62be
Stefano <stefanop21@outlook.es>
2024-02-27 16:51:00 -0500

    update(src/journal/components): Activar una nota en SideBarItem.jsx

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -7,15 +7,23 @@ import {
   ListItemText,
 } from '@mui/material';
 import { useMemo } from 'react';
+import { useDispatch } from 'react-redux';
+import { setActiveNote } from '../../store/journal';
 
-export const SideBarItem = ({ id, title, body, date }) => {
+export const SideBarItem = ({ id, title, body, date, imageUrls = [] }) => {
+  const dispatch = useDispatch();
+
   const newTitle = useMemo(() => {
     return id.length > 10 ? id.slice(0, 10) + '...' : id;
   });
 
+  const onActiveNote = () => {
+    dispatch(setActiveNote({ id, title, body, date, imageUrls }));
+  };
+
   return (
     <ListItem key={id} disablePadding>
-      <ListItemButton>
+      <ListItemButton onClick={onActiveNote}>
         <ListItemIcon>
           <TurnedInNot />
         </ListItemIcon>
