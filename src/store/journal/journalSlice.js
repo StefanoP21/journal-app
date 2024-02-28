96cb3e36cae6e92e6e50a18d4a3f0c37956df6d5
Stefano <stefanop21@outlook.es>
2024-02-27 19:07:55 -0500

    update(src/store/journal/journalSlice.js): Nuevos reducers setSaving y updateNote

--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -26,9 +26,19 @@ export const journalSlice = createSlice({
       state.notes = action.payload;
     },
 
-    setSaving: (state, action) => {},
+    setSaving: (state, action) => {
+      state.isSaving = true;
+    },
 
-    updateNote: (state, action) => {},
+    updateNote: (state, action) => {
+      state.isSaving = false;
+      state.notes = state.notes.map((note) => {
+        if (note.id === action.payload.id) {
+          return action.payload;
+        }
+        return note;
+      });
+    },
 
     deleteNoteById: (state, action) => {},
   },
