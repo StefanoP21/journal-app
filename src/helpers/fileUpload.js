df09931a0b43b156e8296f970d0d3ae4f26481f4
Stefano <stefanop21@outlook.es>
2024-02-29 19:10:15 -0500

    refactor(src/helpers/fileUpload.js): Remover console.log()

--- src/helpers/fileUpload.js
+++ src/helpers/fileUpload.js
@@ -13,13 +13,9 @@ export const fileUpload = async (file) => {
       body: formData,
     });
 
-    console.log(resp);
-
     if (!resp.ok) throw new Error('Error uploading file');
 
     const cloudResp = await resp.json();
-
-    console.log({ cloudResp });
 
     return cloudResp.secure_url;
   } catch (error) {
