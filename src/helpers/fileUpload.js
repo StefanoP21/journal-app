c6dc779cd43303f1da773e025967bc698216d454
Stefano <stefanop21@outlook.es>
2024-02-29 21:01:44 -0500

    refactor(src/helpers/fileUpload.js): Variables de entorno

--- src/helpers/fileUpload.js
+++ src/helpers/fileUpload.js
@@ -1,7 +1,7 @@
 export const fileUpload = async (file) => {
   if (!file) throw new Error('No file selected');
 
-  const cloudUrl = 'https://api.cloudinary.com/v1_1/dkgkw3suh/upload';
+  const cloudUrl = import.meta.env.VITE_VERCEL_CLOUDINARY_URL;
 
   const formData = new FormData();
   formData.append('upload_preset', 'react-journal');
