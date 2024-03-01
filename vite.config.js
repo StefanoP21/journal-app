39a3dd68cf772fb73de15d9d3e9e57f4942509a5
Stefano <stefanop21@outlook.es>
2024-02-29 22:48:37 -0500

    update(vite.config.js): Cofiguración de Vite

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv('mock', process.cwd(), '');
-
-  const processEnvValues = {
-    'process.env': Object.entries(env).reduce((prev, [key, value]) => {
-      return {
-        ...prev,
-        [key]: value,
-      };
-    }, {}),
-  };
-
-  return {
-    plugins: [react()],
-    define: processEnvValues,
-  };
+export default defineConfig({
+  plugins: [react()],
 });
